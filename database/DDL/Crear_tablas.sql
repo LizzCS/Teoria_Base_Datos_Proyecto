1271d1a7c152d9b2a6ffe32d20c42d99ea32409b
Cristina <cristinalizeth_maldonado@unitec.edu>
2026-03-02 17:11:59 -0600

    se añadio el campo auditorio

--- database/DDL/Crear_tablas.sql
+++ database/DDL/Crear_tablas.sql
@@ -1,7 +1,6 @@
 USE Sistema_bancario;
 GO
 
-
 CREATE TABLE usuario (
   usuario_id INT PRIMARY KEY IDENTITY (1,1),
   nombre VARCHAR(100) NOT NULL,
@@ -10,8 +9,12 @@ CREATE TABLE usuario (
   contrasena VARCHAR(255) NOT NULL,
   fecha_registro DATETIME NOT NULL DEFAULT GETDATE(),
   salario_mensual DECIMAL(10, 2) NOT NULL CHECK (salario_mensual >= 0),
-  estado_usuario  BIT NOT NULL DEFAULT 1
-  );
+  estado_usuario BIT NOT NULL DEFAULT 1,
+  creado_por INT NULL,
+  modificado_por INT NULL,
+  creado_en DATETIME NOT NULL DEFAULT GETDATE(),
+  modificado_en DATETIME NOT NULL DEFAULT GETDATE()
+);
 
 CREATE TABLE presupuesto (
   presupuesto_id INT PRIMARY KEY IDENTITY (1,1),
@@ -27,18 +30,23 @@ CREATE TABLE presupuesto (
   total_ahorros DECIMAL(15, 2) NOT NULL,
   fecha_creacion DATETIME NOT NULL DEFAULT GETDATE(),
   estado_presupuesto VARCHAR(20) NOT NULL CHECK (estado_presupuesto IN ('activo', 'cerrado', 'borrador')),
-  CHECK (
-        anio_fin > anio_inicio OR
-        (anio_fin = anio_inicio AND mes_fin >= mes_inicio)
-    )
-  );
+  CHECK (anio_fin > anio_inicio OR (anio_fin = anio_inicio AND mes_fin >= mes_inicio)),
+  creado_por INT NULL,
+  modificado_por INT NULL,
+  creado_en DATETIME NOT NULL DEFAULT GETDATE(),
+  modificado_en DATETIME NOT NULL DEFAULT GETDATE()
+);
 
 CREATE TABLE categoria (
   id_categoria INT PRIMARY KEY IDENTITY (1,1),
   nombre VARCHAR(100) NOT NULL,
   descripcion VARCHAR(255) NULL,
-  tipo_categoria VARCHAR(20) NOT NULL CHECK (tipo_categoria IN ('ingreso', 'gasto', 'ahorro'))
-  );
+  tipo_categoria VARCHAR(20) NOT NULL CHECK (tipo_categoria IN ('ingreso', 'gasto', 'ahorro')),
+  creado_por INT NULL,
+  modificado_por INT NULL,
+  creado_en DATETIME NOT NULL DEFAULT GETDATE(),
+  modificado_en DATETIME NOT NULL DEFAULT GETDATE()
+);
 
 CREATE TABLE subcategoria(
   id_subcategoria INT PRIMARY KEY IDENTITY (1,1),
@@ -47,7 +55,11 @@ CREATE TABLE subcategoria(
   nombre VARCHAR(100) NOT NULL,
   descripcion VARCHAR(255) NULL,
   es_activa BIT NOT NULL DEFAULT 1,
-  es_sub_defecto BIT NOT NULL DEFAULT 1
+  es_sub_defecto BIT NOT NULL DEFAULT 1,
+  creado_por INT NULL,
+  modificado_por INT NULL,
+  creado_en DATETIME NOT NULL DEFAULT GETDATE(),
+  modificado_en DATETIME NOT NULL DEFAULT GETDATE()
 );
 
 CREATE TABLE presupuesto_detalle (
@@ -57,37 +69,49 @@ CREATE TABLE presupuesto_detalle (
   id_subcategoria INT NOT NULL,
   FOREIGN KEY (id_subcategoria) REFERENCES subcategoria(id_subcategoria),
   monto_mensual NUMERIC(12,2) NOT NULL CHECK (monto_mensual >= 0),
-  observaciones VARCHAR(255) NULL
+  observaciones VARCHAR(255) NULL,
+  creado_por INT NULL,
+  modificado_por INT NULL,
+  creado_en DATETIME NOT NULL DEFAULT GETDATE(),
+  modificado_en DATETIME NOT NULL DEFAULT GETDATE()
 );
 
 CREATE TABLE obligacion_fija (
-    id_obligacion INT PRIMARY KEY IDENTITY (1,1),
-    id_subcategoria INT NOT NULL,
-    FOREIGN KEY (id_subcategoria) REFERENCES subcategoria(id_subcategoria),
-    nombre VARCHAR(100) NOT NULL,
-    monto DECIMAL(12, 2) NOT NULL CHECK (monto >= 0),
-    fecha_inicio DATE NOT NULL,
-    fecha_final DATE NULL,
-    dia_mes INT NOT NULL CHECK (dia_mes BETWEEN 1 AND 28),
-    es_vigente BIT NOT NULL DEFAULT 1,
-    CONSTRAINT chk_fecha_final CHECK (fecha_final IS NULL OR fecha_final > fecha_inicio)
+  id_obligacion INT PRIMARY KEY IDENTITY (1,1),
+  id_subcategoria INT NOT NULL,
+  FOREIGN KEY (id_subcategoria) REFERENCES subcategoria(id_subcategoria),
+  nombre VARCHAR(100) NOT NULL,
+  monto DECIMAL(12, 2) NOT NULL CHECK (monto >= 0),
+  fecha_inicio DATE NOT NULL,
+  fecha_final DATE NULL,
+  dia_mes INT NOT NULL CHECK (dia_mes BETWEEN 1 AND 28),
+  es_vigente BIT NOT NULL DEFAULT 1,
+  CONSTRAINT chk_fecha_final CHECK (fecha_final IS NULL OR fecha_final > fecha_inicio),
+  creado_por INT NULL,
+  modificado_por INT NULL,
+  creado_en DATETIME NOT NULL DEFAULT GETDATE(),
+  modificado_en DATETIME NOT NULL DEFAULT GETDATE()
 );
 
 CREATE TABLE transaccion (
-    id_transaccion INT PRIMARY KEY IDENTITY (1,1),
-    id_presupuesto_detalle INT NOT NULL,
-    FOREIGN KEY (id_presupuesto_detalle) REFERENCES presupuesto_detalle(id_presupuesto_detalle),
-    monto DECIMAL(12, 2) NOT NULL CHECK (monto >= 0),
-    metodo_pago VARCHAR(50) NOT NULL CHECK (metodo_pago IN ('efectivo', 'tarjeta debito', 'tarjeta credito', 'transferencia')),
-    fecha DATETIME NOT NULL DEFAULT GETDATE(),
-    descripcion VARCHAR(255) NULL,
-    anio INT NOT NULL,
-    mes INT NOT NULL CHECK (mes BETWEEN 1 AND 12)
+  id_transaccion INT PRIMARY KEY IDENTITY (1,1),
+  id_presupuesto_detalle INT NOT NULL,
+  FOREIGN KEY (id_presupuesto_detalle) REFERENCES presupuesto_detalle(id_presupuesto_detalle),
+  monto DECIMAL(12, 2) NOT NULL CHECK (monto >= 0),
+  metodo_pago VARCHAR(50) NOT NULL CHECK (metodo_pago IN ('efectivo', 'tarjeta debito', 'tarjeta credito', 'transferencia')),
+  fecha DATETIME NOT NULL DEFAULT GETDATE(),
+  descripcion VARCHAR(255) NULL,
+  anio INT NOT NULL,
+  mes INT NOT NULL CHECK (mes BETWEEN 1 AND 12),
+  creado_por INT NULL,
+  modificado_por INT NULL,
+  creado_en DATETIME NOT NULL DEFAULT GETDATE(),
+  modificado_en DATETIME NOT NULL DEFAULT GETDATE()
 );
 
 CREATE TABLE obligacion_transaccion(
-    id_transaccion INT PRIMARY KEY,
-    id_obligacion INT NOT NULL,
-    FOREIGN KEY (id_transaccion) REFERENCES transaccion(id_transaccion),
-    FOREIGN KEY (id_obligacion) REFERENCES obligacion_fija(id_obligacion)
+  id_transaccion INT PRIMARY KEY,
+  id_obligacion INT NOT NULL,
+  FOREIGN KEY (id_transaccion) REFERENCES transaccion(id_transaccion),
+  FOREIGN KEY (id_obligacion) REFERENCES obligacion_fija(id_obligacion),
 );
