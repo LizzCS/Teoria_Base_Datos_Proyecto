473561ed96b2bd4871feeffde0dd55f466d7c089
Cristina <cristinalizeth_maldonado@unitec.edu>
2026-03-01 18:45:51 -0600

    arreglar tablas

--- database/DDL/Crear_tablas.sql
+++ database/DDL/Crear_tablas.sql
@@ -1,5 +1,7 @@
 USE proyecto_tbd;
 
+-- Crear tablas
+
 CREATE TABLE usuario (
   usuario_id INT PRIMARY KEY IDENTITY (1,1),
   nombre VARCHAR(100) NOT NULL,
@@ -23,7 +25,7 @@ CREATE TABLE presupuesto (
   total_ingresos DECIMAL(15, 2) NOT NULL,
   total_gastos DECIMAL(15, 2) NOT NULL,
   total_ahorros DECIMAL(15, 2) NOT NULL,
-  fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  fecha_creacion DATETIME NOT NULL DEFAULT GETDATE(),
   estado_presupuesto VARCHAR(20) NOT NULL CHECK (estado_presupuesto IN ('activo', 'cerrado', 'borrador')),
   CHECK (
         anio_fin > anio_inicio OR
@@ -59,4 +61,33 @@ CREATE TABLE presupuesto_detalle (
 );
 
 CREATE TABLE obligacion_fija (
+    id_obligacion INT PRIMARY KEY IDENTITY (1,1),
+    id_subcategoria INT NOT NULL,
+    FOREIGN KEY (id_subcategoria) REFERENCES subcategoria(id_subcategoria),
+    nombre VARCHAR(100) NOT NULL,
+    monto DECIMAL(12, 2) NOT NULL CHECK (monto >= 0),
+    fecha_inicio DATE NOT NULL,
+    fecha_final DATE NULL,
+    dia_mes INT NOT NULL CHECK (dia_mes BETWEEN 1 AND 28),
+    es_vigente BIT NOT NULL DEFAULT 1,
+    CONSTRAINT chk_fecha_final CHECK (fecha_final IS NULL OR fecha_final > fecha_inicio)
+);
+
+CREATE TABLE transaccion (
+    id_transaccion INT PRIMARY KEY IDENTITY (1,1),
+    id_presupuesto_detalle INT NOT NULL,
+    FOREIGN KEY (id_presupuesto_detalle) REFERENCES presupuesto_detalle(id_detalle),
+    monto DECIMAL(12, 2) NOT NULL CHECK (monto >= 0),
+    metodo_pago VARCHAR(50) NOT NULL CHECK (metodo_pago IN ('efectivo', 'tarjeta debito', 'tarjeta credito', 'transferencia')),
+    fecha DATETIME NOT NULL DEFAULT GETDATE(),
+    descripcion VARCHAR(255) NULL,
+    anio INT NOT NULL,
+    mes INT NOT NULL CHECK (mes BETWEEN 1 AND 12)
+);
+
+CREATE TABLE obligacion_transaccion(
+    id_transaccion INT PRIMARY KEY,
+    id_obligacion INT NOT NULL,
+    FOREIGN KEY (id_transaccion) REFERENCES transaccion(id_transaccion),
+    FOREIGN KEY (id_obligacion) REFERENCES obligacion_fija(id_obligacion)
 );
