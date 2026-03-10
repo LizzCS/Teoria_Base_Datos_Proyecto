693b9f4e3b6a3b0d74294722282bef126a608f3e
Cristina <cristinalizeth_maldonado@unitec.edu>
2026-03-09 18:45:01 -0600

    se actualizo categoria

--- database/procedimientos/crud_categoria.sql
+++ database/procedimientos/crud_categoria.sql
@@ -2,101 +2,91 @@ USE sistema_bancario;
 GO
 
 -- INSERTAR
-CREATE OR ALTER PROCEDURE sp_insertar_subcategoria
-	@id_categoria INT,
-	@nombre_subcategoria VARCHAR(100),
-	@descripcion VARCHAR(255),
-	@es_sub_Defecto BIT,
-	@creado_por INT,
-	@modificado_por INT
-
+CREATE OR ALTER PROCEDURE sp_insertar_categoria
+	@p_nombre VARCHAR (100),
+	@p_descripcion VARCHAR (255),
+	@p_tipo_categoria VARCHAR (50),
+	@p_creado_por INT
 	AS
-	BEGIN
-
-	INSERT INTO dbo.subcategoria (id_categoria, nombre, descripcion, subcategoria_por_defecto, creado_por, modificado_por, creado_en, modificado_en)
-		VALUES (@id_categoria, @nombre_subcategoria, @descripcion, @es_sub_Defecto, @creado_por, @modificado_por, GETDATE(), GETDATE());
-	END
+	BEGIN 
+		INSERT INTO dbo.categoria (nombre, descripcion, tipo_categoria, creado_por, creado_en)
+		VALUES (@p_nombre, @p_descripcion, @p_tipo_categoria, @p_creado_por, GETDATE());
+END
 GO
 
 -- ACTUALIZAR
-CREATE OR ALTER PROCEDURE  sp_actualizar_subcategoria
-	@p_id_subcategoria INT, 
-	@p_nombre VARCHAR (100), 
-	@p_descripcion VARCHAR (255), 
+CREATE OR ALTER PROCEDURE sp_actualizar_categoria
+	@p_id_categoria INT,
+	@p_nombre VARCHAR (100),
+	@p_descripcion VARCHAR (255),
 	@p_modificado_por INT
 	AS
 	BEGIN
-
-	IF NOT EXISTS (SELECT 1 FROM dbo.subcategoria sb WHERE sb.id_categoria = @p_id_subcategoria)
+		IF NOT EXISTS (SELECT 1 FROM dbo.categoria c WHERE c.id_categoria = @p_id_categoria)
 		BEGIN
-			RAISERROR('La subcategoría con ID %d no existe.', 16, 1, @p_id_subcategoria);
+			RAISERROR('La categoría con ID %d no existe.', 16, 1, @p_id_categoria);
 			RETURN;
 		END
 
-	UPDATE dbo.subcategoria
-	SET nombre = @p_nombre,
-		descripcion = @p_descripcion,
-		modificado_por = @p_modificado_por,
-		modificado_en = GETDATE()
-	WHERE id_subcategoria = @p_id_subcategoria;
+		UPDATE dbo.categoria
+		SET nombre = @p_nombre,
+			descripcion = @p_descripcion,
+			modificado_por = @p_modificado_por,
+			modificado_en = GETDATE()
+		WHERE id_categoria = @p_id_categoria;
 	END
 GO
 
--- ELIMINAE
-CREATE OR ALTER PROCEDURE sp_eliminar_subcategoria
-	@p_id_subcategoria INT
+-- ELIMINAR
+CREATE OR ALTER PROCEDURE sp_eliminar_categoria
+	@p_id_categoria INT
 	AS
 	BEGIN 	
-	IF NOT EXISTS (SELECT 1 FROM dbo.subcategoria sb WHERE sb.id_categoria = @p_id_subcategoria)
+		IF NOT EXISTS (SELECT 1 FROM dbo.categoria c WHERE c.id_categoria = @p_id_categoria)
 		BEGIN
-			RAISERROR('La subcategoría con ID %d no existe.', 16, 1, @p_id_subcategoria);
+			RAISERROR('La categoría con ID %d no existe.', 16, 1, @p_id_categoria);
 			RETURN;
 		END
-	IF EXISTS (SELECT 1 FROM dbo.presupuesto_detalle pd WHERE pd.id_subcategoria = @p_id_subcategoria)
-		BEGIN
-			RAISERROR('No se puede eliminar la subcategoría con ID %d porque está asociada a un presupuesto detalle.', 16, 1, @p_id_subcategoria);
-			RETURN;
-		END
-
-	DELETE FROM dbo.subcategoria
-	WHERE id_subcategoria = @p_id_subcategoria;
+		DELETE FROM dbo.categoria
+		WHERE id_categoria = @p_id_categoria;
 	END
 GO
 
 -- CONSULTAR
-CREATE OR ALTER PROCEDURE sp_consultar_subcategoria
-	@p_id_subcategoria INT
+CREATE OR ALTER PROCEDURE sp_consultar_categoria
+	@p_id_categoria INT
 	AS
-	BEGIN 
-		IF NOT EXISTS (SELECT 1 FROM dbo.subcategoria sb WHERE sb.id_subcategoria = @p_id_subcategoria)
+	BEGIN
+		IF NOT EXISTS (SELECT 1 FROM dbo.categoria c WHERE c.id_categoria = @p_id_categoria)
 		BEGIN
-			RAISERROR('La subcategoría con ID %d no existe.', 16, 1, @p_id_subcategoria);
+			RAISERROR('La categoría con ID %d no existe.', 16, 1, @p_id_categoria);
 			RETURN;
 		END
-		SELECT id_subcategoria, id_categoria, nombre, descripcion, 
-		  CASE es_activo 
-			WHEN 1 THEN 'Activo' ELSE 'Inactivo' END AS estado
-		, CASE subcategoria_por_defecto 
-			WHEN 1 THEN 'Es subcategoria por defecto' ELSE 'NO es subcategoria por defecto' END AS estado
-		, creado_por, modificado_por, creado_en, modificado_en, creado_en, modificado_en
-		FROM subcategoria sb
-		WHERE sb.id_subcategoria = @p_id_subcategoria;
+		SELECT id_categoria, nombre, descripcion, tipo_categoria, creado_por, modificado_por, creado_en, modificado_en
+		FROM dbo.categoria
+		WHERE id_categoria = @p_id_categoria;
 	END
 GO
 
 -- LISTAR
-CREATE OR ALTER PROCEDURE sp_listar_subcategorias_por_categoria
-	@p_id_categoria INT
+CREATE OR ALTER PROCEDURE sp_listar_categorias
+	@p_id_usuario INT,
+	@p_tipo_categoria VARCHAR (20)
 	AS
 	BEGIN
-		IF NOT EXISTS (SELECT 1 FROM dbo.categoria sb WHERE sb.id_categoria = @p_id_categoria)
+		IF NOT EXISTS (SELECT 1 FROM dbo.usuario WHERE usuario_id = @p_id_usuario)
 		BEGIN
-			RAISERROR('La categoria con ID %d no existe.', 16, 1, @p_id_categoria);
+			RAISERROR('El usuario con ID %d no existe.', 16, 1, @p_id_usuario);
 			RETURN;
 		END
-			SELECT c.id_categoria, c.nombre, sb.id_subcategoria, sb.nombre
-			FROM categoria c
-			INNER JOIN dbo.subcategoria sb 
-				ON sb.id_categoria = c.id_categoria; 
-	END
+		SELECT DISTINCT c.tipo_categoria, p.usuario_id
+		FROM categoria c
+		INNER JOIN dbo.subcategoria sb 
+			ON c.id_categoria = sb.id_categoria
+		INNER JOIN dbo.presupuesto_detalle pd 
+			ON sb.id_subcategoria = pd.id_subcategoria
+		INNER JOIN dbo.presupuesto p
+			ON pd.id_presupuesto = p.presupuesto_id
+			WHERE p.usuario_id = @p_id_usuario
+	END 
 GO
